c73a19a90d19ab721cdc89bd124388664d74b22b
apian <2766696866@qq.com>
2026-07-13 16:48:06 +0800

    添加 前端初始化 - 使用脚手架生成项目，刚生成项目，未进行任何更改

--- src/main/java/com/xigua/xiguaaicode/XiguaAiCodeApplication.java
+++ src/main/java/com/xigua/xiguaaicode/XiguaAiCodeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XiguaAiCodeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XiguaAiCodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XiguaAiCodeApplication.class, args);
+    }
 
 }
