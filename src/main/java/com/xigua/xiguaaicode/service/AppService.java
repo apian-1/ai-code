c2b78b888594cb11f40929572426f12b6c5e6d13
apian <2766696866@qq.com>
2026-07-16 16:53:23 +0800

    应用模块（完成基本功能） 1. AI 生成基本功能代码 2. 完成基本功能、人工测试通过

--- src/main/java/com/xigua/xiguaaicode/service/AppService.java
+++ src/main/java/com/xigua/xiguaaicode/service/AppService.java
@@ -1,7 +1,12 @@
 package com.xigua.xiguaaicode.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.xigua.xiguaaicode.model.dto.app.AppQueryRequest;
 import com.xigua.xiguaaicode.model.entity.App;
+import com.xigua.xiguaaicode.model.vo.AppVO;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,26 @@ import com.xigua.xiguaaicode.model.entity.App;
  * @author <a href="https://github.com/apian-1">程序员西瓜</a>
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    public QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
 }
